cc85005ad42b7f0a6939a7af9c5ca648ad034d6f
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-04 17:41:03 -0400

    updating logo and css for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,15 +8,15 @@ function Header() {
         <Navbar bg="dark" data-bs-theme="dark">
         <Container>
             <Navbar.Brand href="#home">
-                <img src={portfolio_logo} height={50} width={50}></img>
+                <img src={portfolio_logo} alt='' height={50} width={50}></img>
             </Navbar.Brand>
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#experience">Work Experience</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#resume">Resume</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#home" className='pink'>Home</Nav.Link>
+            <Nav.Link href="#about" className='pink'>About</Nav.Link>
+            <Nav.Link href="#experience" className='pink'>Work Experience</Nav.Link>
+            <Nav.Link href="#projects" className='pink'>Projects</Nav.Link>
+            <Nav.Link href="#resume" className='pink'>Resume</Nav.Link>
+            <Nav.Link href="#contact" className='pink'>Contact</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
