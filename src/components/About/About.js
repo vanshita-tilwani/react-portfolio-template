3866706ee63997594cd8ae83b732537ff08dc481
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-05 23:26:11 -0400

    updating css for about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ import {
     MDBCard
   } from "mdbreact";
 import { motion } from 'framer-motion';
-import {PersonRaisedHand, Laptop, Buildings} from 'react-bootstrap-icons'
+import {PersonRaisedHand, Laptop, Buildings, CodeSlash, PersonWorkspace} from 'react-bootstrap-icons'
 import about_logo from '../../assets/about_logo.png'
 import './About.css';
 
@@ -14,7 +14,7 @@ function About() {
     return(
         <div className="about-section">
          <MDBContainer className="max-dimension">
-            <MDBRow className="justify-content-center text-center max-dimension">
+            <MDBRow className="justify-content-center text-center">
                 <MDBCol className="justify-content-center text-center p-5">
                     <motion.div
                     whileHover={{ scale: 1.2 }} 
@@ -37,10 +37,10 @@ function About() {
                         I am currently pursuing a Master's degree <Laptop className="icon-color"/> in Computer Science at Northeastern University in Boston <Buildings className="icon-color"/>.
                         </div>
                         <div className="mb-2">
-                        My interests lie in Fullstack Engineering, UI Development, Backend Development.
+                        My interests lie in Fullstack Engineering, UI Development, Backend Development. <CodeSlash className="icon-color"/>
                         </div>
                         <div className="mb-2">
-                        Prior to beginning my studies at Northeastern, I spent fours years working as a Software Engineer at companies like ServiceNow and Planful.
+                        Prior to beginning my studies at Northeastern, I spent about fours years working as a Software Engineer <PersonWorkspace className="icon-color"/>  at companies like ServiceNow and Planful.
                         </div>
                         </MDBCardBody>
                         </MDBCard>
@@ -62,8 +62,8 @@ function About() {
                     }}>
                         <img
                   src={about_logo}
-                  width={500}
-                  height={500}
+                  width={400}
+                  height={450}
                   alt=""
                   />
 
