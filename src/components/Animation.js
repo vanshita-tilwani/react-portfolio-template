d4b8fbf3e5e2f2e346864baba1dc96df26e1b042
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-06 17:36:00 -0400

    increasing animation scale to 1.3

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 function Animation({ children }) {
     return (
         <motion.div
-            whileHover={{ scale: 1.2 }}
+            whileHover={{ scale: 1.3 }}
             whileTap={{ scale: 0.8 }}
             initial={{ scale: 0 }}
             animate={{ rotate: 360, scale: 1, repeat: Infinity }}
