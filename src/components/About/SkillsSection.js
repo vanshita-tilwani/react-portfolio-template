6e26854a98962ffd7c816386e2126a19689c4ec4
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-06 11:46:21 -0400

    first draft for Skill Section with only heading for now

--- src/components/About/SkillsSection.js
+++ src/components/About/SkillsSection.js
@@ -1,10 +1,26 @@
+import {
+    MDBCol,
+    MDBRow,
+    MDBCardBody,
+    MDBCard
+  } from "mdbreact";
+import Transition from "../Transition";
+import './style.css';
 
 function SkillsSection() {
 
     return(
-        <div>
-            <strong>Skills Sections!!!!</strong>
-        </div>
+        <MDBRow className="justify-content-center text-center">
+            <MDBCol className="justify-content-center text-center p-5">
+                <Transition>
+                    <MDBCard  className="card-details card-light-background">
+                        <MDBCardBody>
+                            <strong>Skills & Proficiencies: A Palette of Abilities</strong>
+                        </MDBCardBody>
+                    </MDBCard>
+                </Transition>
+            </MDBCol>
+        </MDBRow>
     );
 }
 
