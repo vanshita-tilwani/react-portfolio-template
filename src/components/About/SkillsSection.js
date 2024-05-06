7fa884762db3c9f2aa6ce8875e2828925e719abe
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-06 16:24:36 -0400

    adding animation on tap and hover on skills/tools

--- src/components/About/SkillsSection.js
+++ src/components/About/SkillsSection.js
@@ -40,10 +40,20 @@ function SkillsSection() {
                     </Variants>
                 </MDBCol>
                 <MDBCol className="justify-content-center text-center p-5">
-                    <Variants elements={[<DiDlang size='3rem' />, <DiRuby size='2rem' />, <SiMysql size='3rem' />, <SiMicrosoftsqlserver size='3rem' />, <SiMongodb size='3rem' />, <SiRedis size='3rem' />]}></Variants>
+                    <Variants elements={[<Animation><DiDlang size='4rem' /></Animation>,
+                    <Animation><DiRuby size='4rem' /></Animation>,
+                    <Animation><SiMysql size='4rem' /></Animation>,
+                    <Animation><SiMicrosoftsqlserver size='4rem' /></Animation>,
+                    <Animation><SiMongodb size='4rem' /></Animation>,
+                    <Animation><SiRedis size='4rem' /></Animation>]}></Variants>
                 </MDBCol>
                 <MDBCol className="justify-content-center text-center p-5">
-                    <Variants elements={[<FaGithub size='3rem' />, <VscVscode size='3rem' />, <SiPostman size='3rem' />, <SiIntellijidea size='2rem' />, <SiElasticsearch size='2.5rem' />, <SiKibana size='2.5rem' />]}></Variants>
+                    <Variants elements={[<Animation><FaGithub size='3rem' /></Animation>,
+                    <Animation><VscVscode size='3rem' /></Animation>,
+                    <Animation><SiPostman size='3rem' /></Animation>,
+                    <Animation><SiIntellijidea size='2rem' /></Animation>,
+                    <Animation><SiElasticsearch size='2.5rem' /></Animation>,
+                    <Animation><SiKibana size='2.5rem' /></Animation>]}></Variants>
                 </MDBCol>
             </MDBRow>
         </div>
