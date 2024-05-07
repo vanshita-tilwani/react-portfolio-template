560138b5709bdbc057a896c07231c667a354d07d
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-07 11:44:29 -0400

    first cut of contact component having slide in transition for email icon with the ability to open email on click

--- src/components/About/ContactSection.js
+++ src/components/About/ContactSection.js
@@ -1,10 +1,24 @@
 import SectionHeading from "./SectionHeading";
+import { MDBCol, MDBRow, } from "mdbreact";
+import { EnvelopeAt } from "react-bootstrap-icons";
+import SlideInTransition from "../SlideInTransition";
+import { openEmail } from "../Util";
+import './style.css';
 
 function ContactSection() {
-
     return (
         <div>
             <SectionHeading heading={"Hit Me Up: Let's Chat (preferably not carrier pigeon)"}></SectionHeading>
+            <MDBRow className="justify-content-center text-center">
+                <MDBCol className="justify-content-center text-center p-3">
+                    <SlideInTransition>
+                        <div className="text-white">
+                            <EnvelopeAt width={80} height={80} onClick={openEmail} size='5rem' />
+                        </div>
+                    </SlideInTransition>
+
+                </MDBCol>
+            </MDBRow>
         </div>
     );
 }
