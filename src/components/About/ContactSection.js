99925332a38bea4f4a1547332c03902a9a7cb436
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-07 15:37:47 -0400

    updating the contact section with email details

--- src/components/About/ContactSection.js
+++ src/components/About/ContactSection.js
@@ -1,5 +1,5 @@
 import SectionHeading from "./SectionHeading";
-import { MDBCol, MDBRow, } from "mdbreact";
+import { MDBCol, MDBRow, MDBCard, MDBCardBody } from "mdbreact";
 import { EnvelopeAt } from "react-bootstrap-icons";
 import SlideInTransition from "../SlideInTransition";
 import { openEmail } from "../Util";
@@ -10,14 +10,23 @@ function ContactSection() {
         <div>
             <SectionHeading heading={"Hit Me Up: Let's Chat (preferably not carrier pigeon)"}></SectionHeading>
             <MDBRow className="justify-content-center text-center">
+                <MDBCol></MDBCol>
                 <MDBCol className="justify-content-center text-center p-3">
                     <SlideInTransition>
-                        <div className="text-white">
-                            <EnvelopeAt width={80} height={80} onClick={openEmail} size='5rem' />
-                        </div>
+                        <MDBCard className="py-5 text-white rounded-circle shadow-2-strong m-4 card-details card-dark-background">
+                            <MDBCardBody>
+                                <div className="text-white">
+                                    <EnvelopeAt className="icon-color p-2" width={80} height={80} onClick={openEmail} size='5rem' />
+                                    <div className="m-2">
+                                        Want to chat? Shoot me an email.<br />
+                                        Whether it's about my projects, ideas for collaboration, or just to say hi, I'm all ears! Looking forward to hearing from you!
+                                    </div>
+                                </div>
+                            </MDBCardBody>
+                        </MDBCard>
                     </SlideInTransition>
-
                 </MDBCol>
+                <MDBCol></MDBCol>
             </MDBRow>
         </div>
     );
