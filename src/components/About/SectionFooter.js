12856b99d7914d590cb21547edf92e159d275b31
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-07 16:47:03 -0400

    introducing a section footer to sort of denote section ending!

---
+++ src/components/About/SectionFooter.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function SectionFooter() {
+    return (
+        <div className="d-flex justify-content-center">
+            <div className="line-divider m-5"></div>
+        </div>
+    );
+}
+
+export default SectionFooter;
