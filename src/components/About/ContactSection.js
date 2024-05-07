8830f5f206ef9e501b844441f7ec916f96a893ed
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-07 19:33:02 -0400

    updating width for contact section

--- src/components/About/ContactSection.js
+++ src/components/About/ContactSection.js
@@ -10,23 +10,23 @@ function ContactSection() {
         <div>
             <SectionHeading heading={"Hit Me Up: Let's Chat (preferably not carrier pigeon)"}></SectionHeading>
             <MDBRow className="justify-content-center text-center">
-                <MDBCol></MDBCol>
-                <MDBCol className="justify-content-center text-center p-3">
-                    <SlideInTransition>
-                        <MDBCard className="py-5 text-white rounded-circle shadow-2-strong m-4 card-details card-dark-background">
-                            <MDBCardBody>
-                                <div className="text-white">
-                                    <EnvelopeAt className="p-2" width={80} height={80} onClick={openEmail} size='5rem' />
-                                    <div className="m-2">
-                                        Want to chat? Shoot me an email.<br />
-                                        Whether it's about my projects, ideas for collaboration, or just to say hi, I'm all ears! Looking forward to hearing from you!
+                <div className="d-flex justify-content-center w-50">
+                    <MDBCol className="justify-content-center text-center p-3">
+                        <SlideInTransition>
+                            <MDBCard className="py-5 text-white rounded-circle shadow-2-strong m-4 card-details card-dark-background">
+                                <MDBCardBody>
+                                    <div className="text-white">
+                                        <EnvelopeAt className="p-2" width={80} height={80} onClick={openEmail} size='5rem' />
+                                        <div className="m-2">
+                                            Want to chat? Shoot me an email.<br />
+                                            Whether it's about my projects, ideas for collaboration, or just to say hi, I'm all ears! Looking forward to hearing from you!
+                                        </div>
                                     </div>
-                                </div>
-                            </MDBCardBody>
-                        </MDBCard>
-                    </SlideInTransition>
-                </MDBCol>
-                <MDBCol></MDBCol>
+                                </MDBCardBody>
+                            </MDBCard>
+                        </SlideInTransition>
+                    </MDBCol>
+                </div>
             </MDBRow>
         </div>
     );
