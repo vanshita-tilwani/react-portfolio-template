6c7bd01c3736c9e93fe3dc61890e04a6ff25222a
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-12 20:49:56 -0400

    scroll to section in about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef } from 'react';
 import BioSection from './BioSection';
 import SkillsSection from './SkillsSection';
 import ContactSection from './ContactSection';
@@ -7,12 +8,28 @@ import {
 import '../../main.css';
 
 function About() {
+
+    const bioSectionRef = useRef(null);
+    const skillsSectionRef = useRef(null);
+    const contactSectionRef = useRef(null);
+
+    useEffect(() => {
+        if (window.location.hash === '#bio-section') {
+            bioSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+        else if (window.location.hash === '#skills-section') {
+            skillsSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+        else if (window.location.hash === '#contact-section') {
+            contactSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, []);
     return (
         <div className="about-container on-animation">
             <MDBContainer className="max-dimension">
-                <BioSection></BioSection>
-                <SkillsSection></SkillsSection>
-                <ContactSection></ContactSection>
+                <div ref={bioSectionRef} id='bio-section'><BioSection></BioSection></div>
+                <div ref={skillsSectionRef} id='skills-section'><SkillsSection></SkillsSection></div>
+                <div ref={contactSectionRef} id='contact-section'><ContactSection></ContactSection></div>
             </MDBContainer>
 
         </div>
