4c6cd740307683568895e4e359e3b32647d388cc
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-12 21:36:38 -0400

    updating the footer component to use radix-ui

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,22 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import { BalloonHeartFill, Github, Linkedin, CCircle } from 'react-bootstrap-icons';
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import { BsCCircle } from "react-icons/bs";
+import { HeartFilledIcon, GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons';
+import '../../main.css'
 
 function Footer() {
   return (
-    <Navbar bg="black" >
-      <Container>
-        <Nav className="ml-auto">
-          <div className='font-css'>Copyright <CCircle className='icon-color'></CCircle> Vanshita Tilwani</div>
-        </Nav>
-        <Nav className="ml-auto">
-          <Nav.Item href="" className='font-css'>Made with love!</Nav.Item>
-          <BalloonHeartFill className='icon-color' />
-        </Nav>
-        <Nav className="ml-auto">
-          <Nav.Link href="https://github.com/vanshita-tilwani"><Github className="me-4 icon-color"></Github></Nav.Link>
-          <Nav.Link href="https://www.linkedin.com/in/vanshita-tilwani/"><Linkedin className='icon-color'></Linkedin></Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+    <NavigationMenu.Root className="side-by-side-components">
+      <NavigationMenu.Item className='NavigationMenuList'>
+        <div className='font-css'>Copyright <BsCCircle className='icon-color' /> Vanshita Tilwani</div>
+      </NavigationMenu.Item>
+      <NavigationMenu.Item className='NavigationMenuList'>
+        <div className='font-css'>Made with love<HeartFilledIcon className='icon-color mb-1' /></div>
+      </NavigationMenu.Item>
+      <NavigationMenu.Item className='NavigationMenuList'>
+        <NavigationMenu.Link href='https://github.com/vanshita-tilwani'><GitHubLogoIcon className="me-4 icon-color" /></NavigationMenu.Link>
+        <NavigationMenu.Link href='https://www.linkedin.com/in/vanshita-tilwani/'><LinkedInLogoIcon className='me-4 icon-color' /></NavigationMenu.Link>
+      </NavigationMenu.Item>
+    </NavigationMenu.Root>
   );
 }
 
