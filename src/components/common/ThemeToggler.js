6b265effec543b07d712e4288b7afb02e11db8da
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-15 17:20:28 -0400

    updating themetoggler using radix ui

--- src/components/common/ThemeToggler.js
+++ src/components/common/ThemeToggler.js
@@ -1,35 +1,49 @@
-import * as React from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
-import '../main.css'
+import React from 'react';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { HamburgerMenuIcon, DotFilledIcon, } from '@radix-ui/react-icons';
 
-const ThemeToggler = () => {
-    const options = {
-        'Neon Pink Theme': 'theme-menu-item-neon-pink',
-        'Neon Purple Theme': 'theme-menu-item-neon-purple',
-    };
-
+function ThemeToggler() {
+    const [theme, setTheme] = React.useState('neon-dark-violet');
     const handleDropdownSelect = (eventKey) => {
+        setTheme(eventKey)
         switch (eventKey) {
-            case 'theme-menu-item-neon-pink':
+            case 'neon-dark-pink':
                 document.documentElement.style.setProperty("--imp-icon-color", "#F535AA");
                 break;
-            case 'theme-menu-item-neon-purple':
+            case 'neon-dark-violet':
                 document.documentElement.style.setProperty("--imp-icon-color", "#c770f0");
                 break;
             default:
                 document.documentElement.style.setProperty("--imp-icon-color", "#c770f0");
         }
     };
-
     return (
-        <Dropdown className="theme-menu" onSelect={handleDropdownSelect}>
-            <Dropdown.Toggle className="theme-menu">Theme Toggler</Dropdown.Toggle>
-            <Dropdown.Menu className="theme-menu">{
-                Object.keys(options).map((option) => (
-                    <Dropdown.Item key={option} eventKey={options[option]} className={options[option]} >{option}</Dropdown.Item>
-                ))}
-            </Dropdown.Menu>
-        </Dropdown>
+        <DropdownMenu.Root onSelect={handleDropdownSelect}>
+            <DropdownMenu.Trigger asChild >
+                <button className="NavigationMenuTrigger" aria-label="Customise options">
+                    <HamburgerMenuIcon />
+                </button>
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Portal>
+                <DropdownMenu.Content className="NavigationMenuViewport" sideOffset={5}>
+                    <DropdownMenu.RadioGroup value={theme} onValueChange={handleDropdownSelect}>
+                        <DropdownMenu.RadioItem className="DropdownMenuRadioItem" value="neon-dark-pink">
+                            <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+                                <DotFilledIcon />
+                            </DropdownMenu.ItemIndicator>
+                            Dark Neon Pink
+                        </DropdownMenu.RadioItem>
+                        <DropdownMenu.RadioItem className="DropdownMenuRadioItem" value="neon-dark-violet">
+                            <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+                                <DotFilledIcon />
+                            </DropdownMenu.ItemIndicator>
+                            Dark Neon Violet
+                        </DropdownMenu.RadioItem>
+                    </DropdownMenu.RadioGroup>
+                </DropdownMenu.Content>
+            </DropdownMenu.Portal>
+        </DropdownMenu.Root>
     );
 };
 
