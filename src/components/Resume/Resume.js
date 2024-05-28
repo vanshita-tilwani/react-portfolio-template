235d176e635460d93a983eeaf9bf350f365f0f44
vanshita-tilwani <msvtilwani@gmail.com>
2024-05-27 21:54:41 -0400

    first cut for resume component

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,9 +1,14 @@
-function Resume() {
+import { Document, Page, pdfjs } from "react-pdf";
+import resume from '../../assets/resume.pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+function Resume() {
     return (
         <div className="resume-container text-white">
-            Resume
-        </div>
+            <Document file={resume} className="d-flex justify-content-center">
+                <Page pageNumber={1} />
+            </Document>
+        </div >
     );
 }
 
